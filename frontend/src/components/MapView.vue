3c27368b7fc4772178168f8d8090de99c7c5b6c1
Anurag Karmakar <rockanurg@gmail.com>
2026-06-24 22:51:51 +0530

    Added heat zones

--- frontend/src/components/MapView.vue
+++ frontend/src/components/MapView.vue
@@ -8,13 +8,12 @@
 import { onMounted } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import 'leaflet.heat'
 
 onMounted(() => {
   // Bengaluru center
   const map = L.map('map').setView([12.9716, 77.5946], 12)
 
-  // OpenStreetMap layer
+  // OpenStreetMap
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
@@ -23,31 +22,44 @@ onMounted(() => {
   ).addTo(map)
 
   // Bengaluru marker
+  L.circle([12.9716, 77.5946], {
+    radius: 3500,
+    color: '#ef4444',
+    fillColor: '#ef4444',
+    fillOpacity: 0.35
+  }).addTo(map)
+
+  // Koramangala
+  L.circle([12.9352, 77.6245], {
+    radius: 2500,
+    color: '#f97316',
+    fillColor: '#f97316',
+    fillOpacity: 0.30
+  }).addTo(map)
+
+  // Whitefield
+  L.circle([12.9698, 77.7500], {
+    radius: 2500,
+    color: '#eab308',
+    fillColor: '#eab308',
+    fillOpacity: 0.30
+  }).addTo(map)
+
+  // Electronic City
+  L.circle([12.8456, 77.6603], {
+    radius: 3000,
+    color: '#dc2626',
+    fillColor: '#dc2626',
+    fillOpacity: 0.35
+  }).addTo(map)
+
+  // Bengaluru marker
   L.marker([12.9716, 77.5946])
     .addTo(map)
-    .bindPopup('<b>Bengaluru</b><br>Temperature: 38°C')
+    .bindPopup(
+      '<b>Bengaluru</b><br>Heat Stress: High<br>Temperature: 38°C'
+    )
 
-  // Heat points
-  const heatPoints = [
-    [12.9716, 77.5946, 0.8], // Central Bengaluru
-    [12.9352, 77.6245, 0.9], // Koramangala
-    [12.8456, 77.6603, 1.0], // Electronic City
-    [12.9698, 77.7500, 0.85], // Whitefield
-    [13.0827, 77.5877, 0.75], // Hebbal
-    [12.9141, 77.6100, 0.7], // Jayanagar
-    [12.9279, 77.6271, 0.9], // BTM Layout
-  ]
-
-  // Heat layer
- const heat = L.heatLayer(heatPoints, {
-  radius: 40,
-  blur: 30,
-  maxZoom: 15
-})
-
-heat.addTo(map)
-
-  // Fix rendering issue
   setTimeout(() => {
     map.invalidateSize()
   }, 100)
